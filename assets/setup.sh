a3fcb878e0ecfe8fa713c2e5abce1ba986eb4bf1
usersina <benna.anis2@gmail.com>
2022-10-20 10:37:42 +0100

    Add init script

---
+++ assets/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# ===================== Configure VNC ===================== #
+# Local debug
+# echo $1 >> /opt/setup.sh.output
+# echo $2 >> /opt/setup.sh.output
+
+Xvfb $2 -screen 0 1366x768x16 &
+RUN x11vnc -passwd TestVNC -display $2 -N -forever &
+echo "exec $1" > ~/.xinitrc && chmod +x ~/.xinitrc
+
+# ================= Configure SQLDeveloper ================= #
+echo 'AddVMOption -Dide.user.dir=/data' >> /opt/sqldeveloper/sqldeveloper/bin/sqldeveloper.conf
+# JavaHome should be automatically set, if not then 
+# echo SetJavaHome /usr/lib/jvm/java-11-openjdk >> /opt/sqldeveloper/sqldeveloper/bin/sqldeveloper.conf
